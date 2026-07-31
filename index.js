f1a894f1a2830e13240f49e9e349207075d3212d
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-07-31 11:15:42 +0530

    url parsing started

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now() } : ${ req.url } new req rec\n`;
+  const myUrl = url.parse(req.url,true);
+  console.log(myUrl);
   fs.appendFile("this.txt", log, (error, result) => {
-    switch(req.url){
+    switch(myUrl.pathname){
       case "/" : res.end("Home");
       break
-      case "/about" : res.end("My name is Himanshu");
+      case "/about" : 
+      const username = myUrl.query.myname;
+      res.end(`My name is ${username}`);
       break
       case "/contact" : res.end("Email : abc@gmail.com");
       break
