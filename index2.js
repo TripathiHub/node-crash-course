78c5b4a944f47e9c749ca066abfcc6b74cca84a4
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-08-01 10:58:09 +0530

    made first rest get api

--- index2.js
+++ index2.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=>{
-   return res.send("hello from home")
-})
-app.get("/about",(req,res)=>{
-   return res.send(`hello ${req.query.name} your age ${req.query.age}`)
-})
-app.listen(8000,()=>{
-    console.log("server started");
-})
+app.get("/", (req, res) => {
+  return res.send("hello from home");
+});
+app.get("/about", (req, res) => {
+  return res.send(`hello ${req.query.name} your age ${req.query.age}`);
+});
+app.listen(8000, () => {
+  console.log("server started");
+});
