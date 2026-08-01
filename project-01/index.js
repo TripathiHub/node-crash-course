6043fefc1efdc9cac0ec558725436a6b04d30630
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-08-01 23:36:19 +0530

    post api integrated

--- project-01/index.js
+++ project-01/index.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
-app.get("/api/users",(req,res)=>{
-    res.json(users);
-})
-app.get("/users",(req,res)=>{
-    const html =
-    ` <ul>
-       ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+app.use(express.urlencoded({ extended: false }));
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users", (req, res) => {
+  const html = ` <ul>
+       ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
        </ul>
-    `
-   res.send(html);
-})
-app.get("/api/users/:id",(req,res)=>{
+    `;
+  res.send(html);
+});
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "succcess", id: users.length });
+  });
+});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find(user=> user.id===id);
+    const user = users.find((user) => user.id === id);
     return res.json(user);
-})
-app.listen(8000,()=>{
-    console.log("server started");
-})
+  })
+  .delete((req, res) => {
+    // todo
+    res.json({ status: "pending" });
+  });
+app.listen(8000, () => {
+  console.log("server started");
+});
