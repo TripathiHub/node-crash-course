2508b48a71cafda3bf5065e6a8beb879a0da23ae
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-08-05 10:53:00 +0530

    MongoDB added also delete and patch api integrate

--- project-01/index.js
+++ project-01/index.js
@@ -1,54 +1,93 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const mongoose = require("mongoose");
 const app = express();
+//PORT
+const PORT = 8000;
+//middle ware
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//connection
+mongoose.connect("mongodb://127.0.0.1:27017/node-project-01").then(() => {
+  console.log("MongoDB connected") }).catch((error) => console.log(error));
+// schema
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
 
-app.use((req,res,next)=>{
+  last_name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  job_title: {
+    type: String,
+  },
+},{timestamps : true});
+// model
+const User = mongoose.model("user",userSchema);
+app.use((req, res, next) => {
   const log = `\n${Date.now()} : ${req.url} : ${req.method} : ${req.ip}`;
-  fs.appendFile("./log.txt",log,(err,data)=>{
-    if(err){
+  fs.appendFile("./log.txt", log, (err, data) => {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       console.log("log written");
     }
     next();
-  })
-})
-app.get("/api/users", (req, res) => {
-  res.json(users);
+  });
 });
-app.get("/users", (req, res) => {
+app.get("/api/users", async (req, res) => {
+  const allDBUser =await User.find({});
+  res.json(allDBUser);
+});
+app.get("/users", async(req, res) => {
+  const allDBUser =  await User.find({});
   const html = ` <ul>
-       ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+       ${allDBUser.map((user) => `<li>${user.first_name} - ${user.email}</li>`).join("")}
        </ul>
     `;
   res.send(html);
 });
-app.post("/api/users", (req, res) => {
+app.post("/api/users",async (req, res) => {
   const body = req.body;
-  if(!body || !body.email || !body.job_title){
-    return res.status(400).send({ error : "Fill required data"});
+  if (!body || !body.email || !body.job_title) {
+    return res.status(400).send({ error: "Fill required data" });
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "succcess", id: users.length });
-  });
+  const result = await User.create({
+    first_name : body.first_name,
+    last_name : body.last_name,
+    email : body.email,
+    gender : body.gender,
+    job_title : body.job_title
+  })
+  return res.status(201).json(result);
 });
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    if(!user){
-       return res.status(404).send({ error : "User not found"})
+  .get(async(req, res) => {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
     }
     return res.json(user);
   })
-  .delete((req, res) => {
-    // todo
-    res.json({ status: "pending" });
+  .delete(async(req, res) => {
+   const deletedUser=await User.findByIdAndDelete(req.params.id);
+    res.json({ status: `successfully deleted: ${deletedUser}`});
+  })
+  .patch(async(req,res)=>{
+  const updatedUser=await User.findByIdAndUpdate(req.params.id,{ last_name : "changed"});
+    return res.json({updatedUser});
   });
-app.listen(8000, () => {
-  console.log("server started");
+app.listen(PORT, () => {
+  console.log("server started :",PORT);
 });
