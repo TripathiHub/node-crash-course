12c5905baf5cd66d3d0af93467568d9e0ef23ddf
TripathiHub <himanshuramtripathi10@gmail.com>
2026-09-01 01:07:50 +0530

    mongodb connected

--- auth-mern-app/backend/index.js
+++ auth-mern-app/backend/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const User = require("./models/user");
 const PORT = process.env.PORT || 9000;
-app.get("/",(req,res)=> res.send("Home"));
-app.listen(PORT,()=>console.log(`server started at ${PORT}`));
+mongoose.connect(process.env.MONGODB_URI).
+    then(() => {
+        console.log("MongoDB Connected");
+    }).catch((error) => console.log("Error in connection", error));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Home"));
+app.post("/sign-in",async (req,res)=>{
+    const user = await User.create({
+        name: req.body.name,
+        password : req.body.password,
+        email : req.body.email
+    });
+    res.status(201).json(user);
+})
+app.listen(PORT, () => console.log(`server started at ${PORT}`));
